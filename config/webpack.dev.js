61879f1f1cba8a4fdfe82c866247097f9a8464ad
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-10-25 13:54:53 +0200

    fix small typo in file extension

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -13,7 +13,7 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.js?$/,
+            test: /\.js$/,
             use: [{
                 loader: 'babel-loader',
                 options: {
